bbc847b0c51ee6a2af4efdd3ed72846256b741a9
dbgoldfain <55030435+dbgoldfain@users.noreply.github.com>
2022-05-16 17:23:31 -0600

    Create robot-config.h

---
+++ 99751A&B&C/99751A/Ol' Chaos Theory/include/robot-config.h
@@ -0,0 +1,12 @@
+using namespace vex;
+using namespace std;
+
+extern brain Brain;
+extern competition Competition;
+extern controller Controller;
+
+extern motor driveMs[2][2], dumperM, inTakeMs[2], armM;
+
+extern motor_group leftG, rightG, inTake;
+
+void vexcodeInit(void);
